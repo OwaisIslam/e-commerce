c808aca6ff276b5cb1b9fb563a4917b2829863f4
Owais Islam <owaisislam@gmail.com>
2020-10-21 20:47:03 -0500

    fixed alias error (currently running)

--- models/index.js
+++ models/index.js
@@ -4,6 +4,7 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+
 // Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: 'category_id'
@@ -17,18 +18,16 @@ Category.hasMany(Product, {
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  as: 'product_tag',
+  as: 'productTag',
   foreignKey: 'product_id'
 })
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
-  as: 'product_tag',
+  as: 'productTag',
   foreignKey: 'tag_id'
 })
-
-
 
 
 module.exports = {
